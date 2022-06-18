3d9f1a8ab9d73c55d9a023b796b6a400ba8a08bd
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-18 16:28:24 +0200

    img added

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Intro.css";
 import { faGithubAlt, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Pic from '../../assets/images/kiki_pic.jpg'
 
 const Intro = () => {
   return (
@@ -17,11 +18,13 @@ const Intro = () => {
             <FontAwesomeIcon icon={faGithubAlt} color="var(--black)" />
           </a>
           <a href="https://www.linkedin.com/in/kristijan-federer/" >
-            <FontAwesomeIcon icon={faLinkedin} color="var(--balck)" />
+            <FontAwesomeIcon icon={faLinkedin} color="var(--balck)"/>
           </a>
         </div>
       </div>
-      <div className="i-right">Oompa loopma dooba dee doo</div>
+      <div className="i-right">
+        <img src={Pic} alt="kiki_pic" width="170rem" height="170rem" />
+      </div>
     </div>
   );
 };
