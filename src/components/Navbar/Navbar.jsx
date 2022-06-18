bda319d23d6ef6ec6fc836b9ab0dab9680d59c52
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-18 14:45:13 +0200

    navbar done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Kristijan</div>
+        <span>toggle</span>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>CV</li>
+            <li>Portfolio</li>
+          </ul>
+        </div>
+        <button className="button n-button">
+          Contact me
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
