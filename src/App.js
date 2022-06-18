3a2e45efb4c698d3fb953d622d6b81cb83bf3afe
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-18 15:59:23 +0200

    intro icons set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar/Navbar"
+import Intro from "./components/Intro/Intro";
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
     </div>
   );
 }
