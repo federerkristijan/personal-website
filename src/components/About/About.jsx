d5b283dc9f122d067a3a036a24d50b6fabb9071c
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-19 15:07:19 +0200

    pause

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="a-wrapper">
+      <div className="a-heading">
+        About me
+      </div>
+      <div className="a-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quidem aspernatur omnis. Dolor sed sint sequi doloremque neque adipisci reiciendis. Voluptatem cupiditate minus asperiores nihil deserunt, quis molestias quo similique!
+      </div>
+    </div>
+
   )
 }
 
