bd2919e5a37ded74a40bebeaa97da62224e7264c
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-22 02:59:17 +0200

    all fixed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,13 +6,12 @@ const About = () => {
     <div className="about-wrapper">
       <div className="a-heading">About me</div>
       <div className="a-text">
-        I am ambitious front-end developer looking for a role in an IT company,
-        with a healthy work enivronment, that would allow me to dive deeper into
-        coding, embrace new creative challenges, and further develop my skills
-        and learn new ones.
-        Digital native, cosmopolite and polyglot, with a
+        Originally from Zagreb, Croatia, based in Berlin, Germany. I'm a digital native, cosmopolite and polyglot, driven by a
         self-managing 'Can-do' mindset and positive attitude, that would be a
         rejuvenating addition to your team!
+        Currrently am looking for front-end developer role in an IT company,
+        with a healthy work enivronment, that would allow me to dive deeper into
+        wonderful world of coding, to embrace new creative challenges, and further develop my skills and learn new ones.
       </div>
     </div>
   );
