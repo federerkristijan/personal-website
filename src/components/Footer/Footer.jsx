dd62056a517a996308b368259db18d840bc1960e
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-13 23:45:59 +0200

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,19 @@
-import React, { useContext} from "react";
+import React from "react";
 import "./Footer.css";
 import Github from "@iconscout/react-unicons/icons/uil-github";
 import LinkedIn from "@iconscout/react-unicons/icons/uil-linkedin";
-import { themeContext } from "../../Context";
 
 const Footer = () => {
-  const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
-
   return (
-    <div className="footer-wrapper"
-    stlye={{ background: darkMode ? "var(--black)" : "var(--white)"}} >
+    <div className="footer-wrapper" >
       <div className="f-content">
         <span>federer.kristijan@gmail.com</span>
         <div className="f-icons">
           <a href="https://github.com/federerkristijan">
-            <Github color={darkMode ? "white" : "black"} size="1.8rem" />
+            <Github fill="white" size="1.8rem" />
           </a>
           <a href="https://www.linkedin.com/in/kristijan-federer/">
-            <LinkedIn color={darkMode ? "white" : "black"} size="1.8rem" />
+            <LinkedIn fill="white" size="1.8rem" />
           </a>
         </div>
       </div>
