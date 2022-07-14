c46e0799dd3325bcba404ee428b53bd01d3e281a
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-14 10:05:09 +0200

    menuitems.js done

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,19 @@
+export const MenuItems = [
+  {
+    title: 'Home',
+    path: '/home',
+    name: 'dropdown-link'
+  },
+
+  {
+    title: 'About',
+    path: '/about',
+    name: 'dropdown-link'
+  },
+
+  {
+    title: 'Portfolio',
+    path: '/portfolio',
+    name: 'dropdown-link'
+  }
+];
