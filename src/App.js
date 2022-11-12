d105b459d60421248530713cda10c464a18ae9da
federerkristijan <federer.kristijan@gmail.com>
2022-11-12 13:30:36 +0100

    cookiesBanner implemented and working

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import { themeContext } from "./Context"
-import { useContext, useEffect } from "react"
-import ReactGA from 'react-ga';
+import { themeContext } from "./Context";
+import { useContext, useEffect } from "react";
+//  credtis to https://github.com/react-ga/react-ga
+import ReactGA from "react-ga";
+// credits to https://github.com/Mastermindzh/react-cookie-consent
+import CookieConsent, { Cookies } from "react-cookie-consent";
 
-import NavbarMenu from "./components/Navbar/NavbarMenu"
-import Footer from "./components/Footer/Footer"
+import NavbarMenu from "./components/Navbar/NavbarMenu";
+import Footer from "./components/Footer/Footer";
 
-import Intro from "./pages/Intro/Intro"
-import About from "./pages/About/About"
-import Portfolio from "./pages/Portfolio/Portfolio"
-import Contact from "./pages/Contact/Contact"
+import Intro from "./pages/Intro/Intro";
+import About from "./pages/About/About";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Contact from "./pages/Contact/Contact";
 
-import './App.css'
-import Skills from "./pages/Skills/Skills"
+import "./App.css";
+import Skills from "./pages/Skills/Skills";
 
 ReactGA.initialize(process.env.REACT_APP_GA_MEASURMENT_ID);
 
 function App() {
-  const theme = useContext(themeContext)
+  const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
 
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
-  }, [])
+  }, []);
 
   return (
-    <div className="App"
-    style={{background: darkMode ? 'var(--black)' : '', color: darkMode ? 'white' : ''}}>
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "var(--black)" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
+      <CookieConsent
+        enableDeclineButton
+        onDecline={() => {
+          alert("nay!");
+        }}
+        setDeclineCookie={true}
+        flipButtons={true}
+        style={{ textAlign: "center", background: "grey" }}
+        overlay={true}
+        buttonStyle={{ borderRadius: "7px" }}
+        declineButtonStyle={{ borderRadius: "7px" }}
+      >
+        🍪 This site uses cookies. 🍪
+      </CookieConsent>
       <NavbarMenu />
       <Intro />
       <Portfolio />
