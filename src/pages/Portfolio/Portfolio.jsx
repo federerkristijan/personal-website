37feb6920d490736a2929bfbe72b397cbaa9ab89
federerkristijan <federer.kristijan@gmail.com>
2022-12-01 09:40:31 +0100

    navbar fixed, id fro about and skills added

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -50,7 +50,7 @@ export default class SimpleSlider extends Component {
     };
 
     return (
-      <div className="portfolio">
+      <div className="portfolio" id="portfolio">
         <h2>Recent Projects</h2>
         <Slider {...settings}>
           <div>
